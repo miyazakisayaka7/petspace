e04b2cadf8eece1aef94cd8db1910ef1a37c60f6
miyazakisayaka7 <une_charrue@yahoo.co.jp>
2021-05-11 16:10:16 +0900

    テーブル削除後

---
+++ db/migrate/20210510074432_create_diaries.rb
@@ -0,0 +1,10 @@
+class CreateDiaries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :diaries do |t|
+      t.date :date,             null: false
+      t.text :text,               null: false
+      t.references :user,         foreign_key: true
+      t.timestamps
+    end
+  end
+end
