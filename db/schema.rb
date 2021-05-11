6194ea2250adf723475f4bfe7f9676d380fce15a
miyazakisayaka7 <une_charrue@yahoo.co.jp>
2021-05-11 16:21:09 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,20 @@ ActiveRecord::Schema.define(version: 2021_05_10_152051) do
 
   create_table "pets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
+    t.string "sex", null: false
+    t.text "type_of_pet", null: false
+    t.date "birthday", null: false
+    t.text "color", null: false
+    t.text "hospital", null: false
+    t.text "medical_history", null: false
+    t.text "medicine", null: false
+    t.text "allergies", null: false
+    t.string "walk_time"
+    t.text "character"
+    t.string "type_of_foods"
+    t.string "number_of_meals"
+    t.string "amount_of_food"
+    t.text "memo"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
